5d93df5e14fb9086fdf5f2e8e76b1377b42af120
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-09-02 13:10:13 +0300

    added test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess =0;
     }
 
-    guess() {
+    setRange(min, max) {
+this.min = min;
+this.max = max;
+this.currentGuess = Math.floor((min + max) / 2);
+   }
 
+    guess() {
+return this.currentGuess;
     }
 
     lower() {
-
+this.max = this.currentGuess - 1;
+this.currentGuess = Math.floor((this.min + this.max) / 2);
     }
 
     greater() {
-
+this.min = this.currentGuess + 1;
+this.currentGuess = Math.floor((this.min + this.max) / 2);
     }
 }
 
