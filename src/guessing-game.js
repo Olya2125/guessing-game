b195f70202cebcc03f224719561cd05a5bdf5261
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-09-16 12:25:01 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,39 +1,20 @@
 class GuessingGame {
-    constructor() {
-        this.min = null;
-        this.max = null;
-        this.lastGuess = null;
-    }
-
-    setRange(min, max) {
-        this.min = min;
-        this.max = max;
-   }
-
-    guess() {
-        if (this.min === null || this.max === null) {
-            throw new Error("Range not set");
-        }
-            this.lastGuess = Math.floor((this.min + this.max) / 2);
-            return this.lastGuess;
-    }
-
-    lower() {
-        if (this.lastGuess === null) {
-            throw new Error("No previous guess");
-          }
-      
-          this.max = this.lastGuess - 1;
-    }
-
-    greater() {
-        if (this.lastGuess === null) {
-            throw new Error("No previous guess");
-          }
-      
-          this.min = this.lastGuess + 1;
-        }
-    }
-
+  constructor() { }
+  setRange(min, max) {
+      this.min = min;
+      this.max = max;
+  }
+  guess() {
+      return Math.round((this.min + this.max) / 2);
+  }
+  lower() {
+      this.max = this.guess();
+  }
+  greater() {
+      this.min = this.guess();
+  }
+}
 
 module.exports = GuessingGame;
+
+
